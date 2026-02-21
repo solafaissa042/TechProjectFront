fe2deeec07abe8b98e32c5c740f2a95a922c6e0c
solafaissa12 <solafaissa1@gmail.com>
2026-02-21 13:39:14 +0200

    new update

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,16 +4,26 @@ const Public = () => {
   const content = (
     <section className="public">
       <header>
-        <h1>
+        <h1 className="welcome">
           Welcome to <span className="nowrap">Dan D. repairs</span>
         </h1>
       </header>
       <main className="public__main">
         <p>Located in a beautiful downtown city</p>
+        <p className="public__main-p">
+          To see more information about us please register and have a beautiful
+          journey
+        </p>
+        <p className="public__main-p">Phone: 001122334455</p>
+        <p className="public__main-p">E-mail: shop@gmail.com</p>
       </main>
       <footer>
-        <Link to="/login"> login</Link>
-        <Link to="/register"> register</Link>
+        <Link className="login" to="/login">
+          login
+        </Link>
+        <Link className="register" to="/register">
+          register
+        </Link>
       </footer>
     </section>
   );
